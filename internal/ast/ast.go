cc0cb52ad6657a9a5f8ef614a538bd6e442d5118
impossibleclone <impossibleclone2005@gmail.com>
2026-02-22 17:11:02 +0530

    parsing infix expressions and the tests.

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -59,8 +59,8 @@ type InfixExpression struct {
 	Right    Expression
 }
 
-func (oe *InfixExpression) expressionNode() {}
-func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal}
+func (oe *InfixExpression) expressionNode()      {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
 func (oe *InfixExpression) String() string {
 	var out bytes.Buffer
 	out.WriteString("(")
